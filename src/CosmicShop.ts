9c2aa8a30a4ff8c0fe07d8f647715a104945d9ec
Hri7566 <hri7566@gmail.com>
2022-10-01 04:50:01 -0400

    update shop

--- src/CosmicShop.ts
+++ src/CosmicShop.ts
@@ -3,8 +3,25 @@
  * Cosmic item shop module
  */
 
+import { EventEmitter } from 'events';
+import { Item, ShopListing } from './CosmicTypes';
+
+const DEFAULT_PRICE = 10;
+
 class CosmicShop {
-    public static itemList = {};
+    protected static itemList: ShopListing[] = [];
+
+    public static getListings(): ShopListing[] {
+        return this.itemList;
+    }
+
+    public static getItemPrice(id: string): number {
+        for (let ls of this.itemList) {
+            if (ls.item.id == id) {
+                return ls.overridePrice || ls.item.value || DEFAULT_PRICE;
+            }
+        }
+    }
 }
 
 export {
