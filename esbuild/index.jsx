3250f9522b6c42fd7dd5a11c0feb53b5863a134b
Hri7566 <hri7566@gmail.com>
2022-10-01 22:22:23 -0400

    timezone fix

--- esbuild/index.jsx
+++ esbuild/index.jsx
@@ -1,2 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/server';
+
 require('./sass/screen.scss');
-console.log('Hello, world!');
+
+let text = ReactDOM.renderToString(<p>hello</p>);
+
+document.getElementsByTagName('app').innerHTML = text;
--- src/CosmicLogger.ts
+++ src/CosmicLogger.ts
@@ -95,11 +95,21 @@ function log_full_info(method = 'log', ...args: any[]) {
     const ms = (new Number(new Date()) as number);
     const ss = Math.floor((ms / 1000) % 60);
     const mm = Math.floor((ms / 1000 / 60) % 60);
-    const hh = Math.floor(((ms / 1000 / 60 / 60) % 12) - (new Date().getTimezoneOffset() / 60));
+    let hh = Math.floor(((ms / 1000 / 60 / 60) % 12) - (new Date().getTimezoneOffset() / 60));
 
-    const ampm = Math.floor((ms / 1000 / 60 / 60) % 24) > 12 ? 'PM' : 'AM';
+    let ampm = Math.floor((ms / 1000 / 60 / 60) % 24) > 12 ? 'PM' : 'AM';
+    
+    if (hh < 0) {
+        hh += 12;
 
-    console[method](`${hh.toString()}:${mm.toString().padStart(2, '0')}:${ss.toString().padStart(2, '0')} ${ampm}`, ...args);
+        if (ampm == 'AM') {
+            ampm = 'PM';
+        } else {
+            ampm = 'AM'
+        }
+    }
+
+    console[method](`${hh.toString().padStart(2, '0')}:${mm.toString().padStart(2, '0')}:${ss.toString().padStart(2, '0')} ${ampm}`, ...args);
 }
 
 /**
