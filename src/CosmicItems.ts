8a2eaf819c5b00108276c07e1836f5892e431ab7
Hri7566 <hri7566@gmail.com>
2022-10-09 04:38:57 -0400

    add max stack and update display name of items

--- src/CosmicItems.ts
+++ src/CosmicItems.ts
@@ -9,12 +9,13 @@ import { Item, FoodItem, AnyItem } from "./CosmicTypes";
 export const ITEMS: Record<string | symbol, AnyItem> = {
     LARGER_EGGS: {
         id: 'upgrade_larger_eggs',
-        displayName: 'Larger Eggs',
+        displayName: 'Upgrade: Larger Eggs',
         emoji: '🥚',
         count: 1,
         description: `Bake slightly faster`,
         value: 150,
         sellable: false,
-        cake_bonus: 1.25
+        cake_bonus: 1.25,
+        max_stack: 1
     },
 }
