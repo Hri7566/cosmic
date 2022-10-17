2c69c40f24b0df911556538af16b9e8f4da66f73
Hri7566 <hri7566@gmail.com>
2022-10-17 14:24:06 -0400

    fix C compilation

--- scripts/sh/build-c.sh
+++ scripts/sh/build-c.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+bash scripts/sh/clean-c.sh
+
+export $(cat .env | xargs)
+
 mkdir -p build/c
 cd build/c
 
-cmake ../..
+# export CC=/usr/bin/clang
+# export CXX=/usr/bin/clang++
+
+cmake ../../c
 make
