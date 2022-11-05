52fbf61b90eecf68d0e726a42d9aeed11e07a51a
Hri7566 <hri7566@gmail.com>
2022-11-05 05:41:51 -0400

    easd

--- src/CosmicItems.ts
+++ src/CosmicItems.ts
@@ -115,5 +115,112 @@ export const ITEMS: Record<string | symbol, AnyItem> = {
         value: 500,
         sellable: false,
         max_stack: 3
+    },
+    CARE_A: {
+        id: 'care_a',
+        displayName: 'Care A',
+        description: "When the emergency began, you were all looking for Care A.",
+        count: 1,
+        value: 0,
+        sellable: false,
+        max_stack: 1
+    },
+    CARE_B: {
+        id: 'care_b',
+        displayName: 'Care B',
+        description: "Care B is scared and pounding on the door.",
+        count: 1,
+        value: 0,
+        sellable: false,
+        max_stack: 1
+    },
+    CARE_NLM: {
+        id: 'care_nlm',
+        displayName: 'Care NLM',
+        description: "Care NLM escaped from the school's basement and wandered the Newmaker Plane for days.",
+        count: 1,
+        value: 0,
+        sellable: false,
+        max_stack: 1
+    },
+    HEROS_BOW: {
+        id: 'bow_hero',
+        displayName: "Hero's Bow",
+        count: 1,
+        value: 0,
+        sellable: false,
+        max_stack: 1
+    },
+    FIRE_ARROW: {
+        id: 'arrow_fire',
+        displayName: "Fire Arrow",
+        count: 1,
+        value: 0,
+        sellable: false,
+        max_stack: 30
+    },
+    ICE_ARROW: {
+        id: 'arrow_ice',
+        displayName: "Ice Arrow",
+        count: 1,
+        value: 0,
+        sellable: false,
+        max_stack: 30
+    },
+    LIGHT_ARROW: {
+        id: 'arrow_light',
+        displayName: "Light Arrow",
+        count: 1,
+        value: 0,
+        sellable: false,
+        max_stack: 30
+    },
+    BOMB: {
+        id: 'bomb',
+        displayName: "Bomb",
+        count: 1,
+        value: 0,
+        sellable: false,
+        max_stack: 20
+    },
+    LENS_OF_TRUTH: {
+        id: 'lens_of_truth',
+        displayName: "Lens of Truth",
+        count: 1,
+        value: 0,
+        sellable: false,
+        max_stack: 1
+    },
+    HOOKSHOT: {
+        id: 'hookshot',
+        displayName: "Hookshot",
+        count: 1,
+        value: 100000,
+        sellable: false,
+        max_stack: 1
+    },
+    GREAT_FAIRY_SWORD: {
+        id: 'sword_great_fairy',
+        displayName: "Great Fairy's Sword",
+        count: 1,
+        value: 10_000_000,
+        sellable: false,
+        max_stack: 1
+    },
+    BOTTLE: {
+        id: 'bottle',
+        displayName: "Bottle",
+        count: 6,
+        value: 500000,
+        max_stack: 1,
+        sellable: false
+    },
+    PICTOGRAPH_BOX: {
+        id: 'pictograph_box',
+        displayName: "Pictograph Box",
+        count: 1,
+        value: 250000,
+        max_stack: 1,
+        sellable: false
     }
 }
